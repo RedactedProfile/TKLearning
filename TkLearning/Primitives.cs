c140cafa5810e240c91b4abaa0cc9fecc35ac84a
Kyle Harrison <redactedprofile@gmail.com>
2022-10-24 17:20:04 -0700

    Trying to get image loading working

--- TkLearning/Primitives.cs
+++ TkLearning/Primitives.cs
@@ -5,6 +5,7 @@ using System.Text;
 using System.Threading.Tasks;
 using OpenTK.Mathematics;
 using OpenTK.Graphics.OpenGL4;
+using StbImageSharp;
 
 namespace TkLearning.Primitives
 {
---
+++ TkLearning/Texture.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.IO;
+using OpenTK.Graphics.OpenGL4;
+using StbImageSharp;
+
+namespace TkLearning
+{
+    public class Texture : IDisposable
+    {
+        int Handle;
+        
+        private bool disposedValue = false;
+
+        public Texture(string path)
+        {
+            StbImage.stbi_set_flip_vertically_on_load(1);
+            ImageResult image = ImageResult.FromStream(File.OpenRead(path), ColorComponents.RedGreenBlueAlpha);
+
+            Handle = GL.GenTexture();
+            Use();
+            GL.TexImage2D(TextureTarget.Texture2D, 0, PixelInternalFormat.Rgba, image.Width, image.Height, 0, PixelFormat.Rgba, PixelType.UnsignedByte, image.Data);
+        }
+
+        public void Use()
+        {
+            //GL.UseProgram(Handle);
+            GL.BindTexture(TextureTarget.Texture2D, Handle);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (!disposedValue)
+            {
+                //GL.DeleteProgram(Handle);
+                disposedValue = true;
+            }
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        ~Texture()
+        {
+            //GL.DeleteProgram(Handle);
+        }
+    }
+}
